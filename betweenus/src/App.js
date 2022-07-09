5dfef5a6c8ce9830ab37f82912cf1ef41de43296
kwaksj329 <kwaksj329@gmail.com>
2022-07-10 02:03:12 +0900

    feat: Add Add-page react hook form

--- betweenus/src/App.js
+++ betweenus/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Add from "./pages/Add";
 import React from "react";
+import Basic from "./pages/Basic";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />}></Route>
         <Route path="/home" element={<Home />}></Route>
         <Route path="/add" element={<Add />}></Route>
+        <Route path="/basic" element={<Basic />}></Route>
       </Routes>
     </Router>
   );
--- betweenus/src/pages/Add.js
+++ betweenus/src/pages/Add.js
@@ -3,8 +3,15 @@ import { Toolbar, Button, TextField, Grid, Typography } from "@mui/material";
 import { Container } from "@mui/system";
 import { Link } from "react-router-dom";
 import ArrowBackIosNewRoundedIcon from "@mui/icons-material/ArrowBackIosNewRounded";
+import { useForm, Controller } from "react-hook-form";
 
-export default function Add() {
+const Add = () => {
+  const { handleSubmit, control } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <Container>
       <Grid item xs={12}>
@@ -17,68 +24,161 @@ export default function Add() {
           </Typography>
         </Toolbar>
       </Grid>
-      <Grid container spacing={3}>
-        <Grid item xs={12}>
-          <TextField
-            id="standard-basic"
-            label="제목"
-            variant="standard"
-            fullWidth
-          />
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Grid container spacing={3}>
+          <Grid item xs={12}>
+            <Controller
+              name="title"
+              control={control}
+              defaultValue=""
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  label="제목"
+                  variant="standard"
+                  id="standard-basic"
+                  value={value}
+                  onChange={onChange}
+                  error={!!error}
+                  fullWidth
+                  helperText={error ? error.message : null}
+                />
+              )}
+              rules={{ required: "모집 제목을 입력해주세요" }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Controller
+              name="time"
+              control={control}
+              defaultValue=""
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  label="주문시간 설정"
+                  variant="standard"
+                  id="standard-basic"
+                  value={value}
+                  onChange={onChange}
+                  error={!!error}
+                  fullWidth
+                  helperText={error ? error.message : null}
+                />
+              )}
+              rules={{ required: "주문시간을 설정해주세요" }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Controller
+              name="diner"
+              control={control}
+              defaultValue=""
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  label="가게 이름"
+                  variant="standard"
+                  id="standard-basic"
+                  value={value}
+                  onChange={onChange}
+                  error={!!error}
+                  fullWidth
+                  helperText={error ? error.message : null}
+                />
+              )}
+              rules={{ required: "가게 이름을 입력해주세요" }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <Controller
+              name="location"
+              control={control}
+              defaultValue=""
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  label="픽업장소"
+                  variant="standard"
+                  id="standard-basic"
+                  value={value}
+                  onChange={onChange}
+                  error={!!error}
+                  fullWidth
+                  helperText={error ? error.message : null}
+                />
+              )}
+              rules={{ required: "픽업장소를 입력해주세요" }}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <Button variant="outlined" fullWidth>
+              주소 찾기
+            </Button>
+          </Grid>
+          <Grid item xs={12}>
+            <Controller
+              name="detailed"
+              control={control}
+              defaultValue=""
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  label="상세 픽업장소"
+                  variant="standard"
+                  id="standard-basic"
+                  value={value}
+                  onChange={onChange}
+                  error={!!error}
+                  fullWidth
+                  helperText={error ? error.message : null}
+                />
+              )}
+              rules={{ required: "상세 픽업장소를 입력해주세요" }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Controller
+              name="info"
+              control={control}
+              render={({
+                field: { onChange, value },
+                fieldState: { error },
+              }) => (
+                <TextField
+                  defaultValue="전달하고 싶은 추가 정보를 입력해주세요 :)"
+                  label="추가 정보"
+                  id="outlined-multiline-static"
+                  multiline
+                  rows={4}
+                  value={value}
+                  onChange={onChange}
+                  error={!!error}
+                  fullWidth
+                  helperText={error ? error.message : null}
+                />
+              )}
+              rules={{ required: "추가 정보를 입력해주세요" }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Button type="submit" variant="contained" fullWidth>
+              모집하기
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="standard-basic"
-            label="주문시간 설정"
-            variant="standard"
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="standard-basic"
-            label="가게 이름"
-            variant="standard"
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <TextField
-            id="standard-basic"
-            label="픽업 장소"
-            variant="standard"
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={6}>
-          <Button variant="outlined" fullWidth>
-            주소 찾기
-          </Button>
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="standard-basic"
-            label="상세 픽업 장소"
-            variant="standard"
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <TextField
-            id="outlined-multiline-static"
-            label="추가 정보"
-            multiline
-            rows={4}
-            defaultValue="전달하고 싶은 추가정보를 입력해주세요 :)"
-            fullWidth
-          />
-        </Grid>
-        <Grid item xs={12}>
-          <Button variant="contained" fullWidth>
-            모집하기
-          </Button>
-        </Grid>
-      </Grid>
+      </form>
     </Container>
   );
-}
+};
+
+export default Add;
