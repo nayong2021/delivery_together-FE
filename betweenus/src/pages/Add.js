cdb74aee5e254eba82da56b2dc1bb4acf81a7c97
kwaksj329 <kwaksj329@gmail.com>
2022-07-21 16:08:48 +0900

    fix: Fix Add page useForm & onClick

--- betweenus/src/pages/Add.js
+++ betweenus/src/pages/Add.js
@@ -1,10 +1,15 @@
 import * as React from "react";
 import MetaTag from "../components/common/MetaTag";
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 const Add = () => {
   const navigate = useNavigate();
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <div className="Add">
@@ -22,63 +27,83 @@ const Add = () => {
         </div>
       </header>
 
-      <section className="item-write">
-        <div className="wrap">
-          <div className="frm2">
-            <div className="frm-group">
-              <input type="text" placeholder="제목" className="inp-frm" />
-            </div>
-
-            <div className="frm-group frm-group-time">
-              <div className="tit-frm">
-                주문시간 <br />
-                설정
-              </div>
-              <div className="inp-group-time">
-                타임시커 라이브러리가 들어갑니다.
-              </div>
-            </div>
-
-            <div className="frm-group">
-              <input type="text" placeholder="가게 이름" className="inp-frm" />
-            </div>
-
-            <div className="frm-group">
-              <div className="inp-group">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <section className="item-write">
+          <div className="wrap">
+            <div className="frm2">
+              <div className="frm-group">
                 <input
                   type="text"
-                  placeholder="픽업 장소"
+                  placeholder="제목"
                   className="inp-frm"
+                  {...register("title")}
                 />
-                <button type="button" className="btn-frm">
-                  주소 찾기
-                </button>
+              </div>
+
+              <div className="frm-group frm-group-time">
+                <div className="tit-frm">
+                  주문시간 <br />
+                  설정
+                </div>
+                <div className="inp-group-time">
+                  <input
+                    type="text"
+                    placeholder="주문시간 설정"
+                    className="inp-frm"
+                    {...register("orderTime")}
+                  />
+                </div>
+              </div>
+
+              <div className="frm-group">
+                <input
+                  type="text"
+                  placeholder="가게 이름"
+                  className="inp-frm"
+                  {...register("storeName")}
+                />
+              </div>
+
+              <div className="frm-group">
+                <div className="inp-group">
+                  <input
+                    type="text"
+                    placeholder="픽업 장소"
+                    className="inp-frm"
+                    {...register("pickupPlace")}
+                  />
+                  <button type="button" className="btn-frm">
+                    주소 찾기
+                  </button>
+                </div>
+              </div>
+
+              <div className="frm-group">
+                <input
+                  type="text"
+                  placeholder="상세 픽업 장소"
+                  className="inp-frm"
+                  {...register("detailPickupPlace")}
+                />
               </div>
             </div>
 
-            <div className="frm-group">
-              <input
-                type="text"
-                placeholder="상세 픽업 장소"
-                className="inp-frm"
-              />
+            <div className="add-info">
+              <div className="tit">추가정보</div>
+              <textarea
+                className="txt"
+                {...register("additionalInfo")}
+              ></textarea>
             </div>
-          </div>
 
-          <div className="add-info">
-            <div className="tit">추가정보</div>
-            <textarea className="txt"></textarea>
-          </div>
-
-          <div className="btn-group-bottom">
-            <Link to="/home">
-              <button type="button" className="btn-custom">
+            <div className="btn-group-bottom">
+              <button type="submit" className="btn-custom">
                 모집하기
               </button>
-            </Link>
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+      </form>
     </div>
   );
 };
