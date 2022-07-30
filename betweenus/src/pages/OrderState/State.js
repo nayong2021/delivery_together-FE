4fb9af91461d7a061f3a98b2b891588c9286f71a
kwaksj329 <kwaksj329@gmail.com>
2022-07-31 07:17:16 +0900

    feat: Add Guest & Host Before Order html, css

--- betweenus/src/pages/OrderState/State.js
+++ betweenus/src/pages/OrderState/State.js
@@ -6,15 +6,19 @@ import JoinBefore from "../../components/OrderState/JoinBefore";
 import HostNoGuest from "../../components/OrderState/HostNoGuest";
 import HostYesGuest from "../../components/OrderState/HostYesGuest";
 import GuestWaitRecruit from "../../components/OrderState/GuestWaitRecruit";
+import HostBeforeOrder from "../../components/OrderState/HostBeforeOrder";
+import GuestBeforeOrder from "../../components/OrderState/GuestBeforeOrder";
 
 const State = () => {
   return (
-    <div className="Map">
+    <div className="State">
       <MetaTag />
       {/* <JoinBefore /> */}
       {/* <HostYesGuest /> */}
       {/* <HostNoGuest /> */}
-      <GuestWaitRecruit />
+      {/* <GuestWaitRecruit /> */}
+      {/* <HostBeforeOrder /> */}
+      <GuestBeforeOrder />
       <BottomNavigation order={"active"} />
     </div>
   );
