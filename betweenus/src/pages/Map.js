9efa3626d2ce01b634e669935445d846d03184aa
kwaksj329 <kwaksj329@gmail.com>
2022-09-15 16:09:02 +0900

    feat: Kakao map sdk

--- betweenus/src/pages/Map.js
+++ betweenus/src/pages/Map.js
@@ -2,12 +2,14 @@ import * as React from "react";
 import "../assets/css/common.css";
 import MetaTag from "../components/common/MetaTag";
 import BottomNavigation from "../components/common/BottomNavigation";
+import KakaoMap from "../components/map/KakaoMap";
 
 const Map = () => {
   return (
     <div className="Map">
       <MetaTag />
       <section className="map" width="100%" height="100%">
+        <KakaoMap />
         <a href="/search" className="btn-search">
           <button type="button" className="btn-location"></button>
         </a>
