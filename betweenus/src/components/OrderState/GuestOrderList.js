108f0d66ca258b2b0d65a7944271f3e2bbba103b
nayong2021 <nayong2017@ajou.ac.kr>
2022-09-29 22:23:37 +0900

    feature: rendering order option on order cart develop completed

--- betweenus/src/components/OrderState/GuestOrderList.js
+++ betweenus/src/components/OrderState/GuestOrderList.js
@@ -2,25 +2,23 @@ import * as React from "react";
 import { ReactComponent as User1 } from "../../assets/img/ico_user1.svg";
 import OrderMenuItem from "./OrderMenuItem";
 
-export default function GuestOrderList({
-    guest
-}) {
-    return (
-        <>
-            <div className="order-info">
-                <div className="user">
-                    <User1 />
-                    &nbsp;{guest.nickName}
-                </div>
-                <div className="total-price">총금액: {guest.memberTotalPrice}원</div>
-            </div>
-            <div className="menu-group">
-            {guest.orderItems.map((menu, idx) => (
-                <OrderMenuItem
-                key={idx}
-                menu={menu}/>
-                ))}
-            </div>
-        </>
-    );
+export default function GuestOrderList({ guest }) {
+  return (
+    <>
+      <div className="order-info">
+        <div className="user">
+          <User1 />
+          &nbsp;{guest.nickName}
+        </div>
+        <div className="total-price">
+          총금액: {guest.memberTotalPrice.toLocaleString()}원
+        </div>
+      </div>
+      <div className="menu-group">
+        {guest.orderItems.map((menu, idx) => (
+          <OrderMenuItem key={idx} menu={menu} />
+        ))}
+      </div>
+    </>
+  );
 }
--- betweenus/src/components/OrderState/OrderResult.js
+++ betweenus/src/components/OrderState/OrderResult.js
@@ -1,30 +1,41 @@
 import * as React from "react";
 import useStoreOrderInfo from "../../store/storeOrderInfo";
 
-export default function ChatItem() {
-
+export default function OrderResult() {
   const { orderInfo } = useStoreOrderInfo();
-  return (
-    <ol className="order-result">
-        <li>
-            <div className="tit">그룹 음식 총 금액</div>
-            <div className="txt">{orderInfo.totalPrice}원</div>
-        </li>
 
-        <li>
+  console.log(orderInfo);
+
+  return (
+    <>
+      {Object.keys(orderInfo).length !== 0 ? (
+        <ol className="order-result">
+          <li>
+            <div className="tit">그룹 음식 총 금액</div>
+            <div className="txt">{orderInfo.totalPrice.toLocaleString()}원</div>
+          </li>
+
+          <li>
             <div className="tit">배달비</div>
             <div className="txt">4,000원</div>
             {/* <div className="txt">{orderInfo.expectedDeliveryFee}원</div> */}
-        </li>
+          </li>
 
-        <li>
+          <li>
             <div className="tit">
-                <strong>총 금액</strong>
+              <strong>총 금액</strong>
             </div>
             <div className="txt">
-                <strong>{orderInfo.totalPrice + orderInfo.expectedDeliveryFee}원</strong>
+              <strong>
+                {(
+                  orderInfo.totalPrice + orderInfo.expectedDeliveryFee
+                ).toLocaleString()}
+                원
+              </strong>
             </div>
-        </li>
-    </ol>
+          </li>
+        </ol>
+      ) : null}
+    </>
   );
 }
