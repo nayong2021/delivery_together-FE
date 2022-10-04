00e96204b9bd90e10984d4d8661ff661a85c67b8
kwaksj329 <kwaksj329@gmail.com>
2022-10-05 05:04:56 +0900

    feat: Add geolocation to mark current location

--- betweenus/src/components/map/KakaoMap.js
+++ betweenus/src/components/map/KakaoMap.js
@@ -1,15 +1,56 @@
 import * as React from "react";
 import { Map, MapMarker } from "react-kakao-maps-sdk";
+import { useState, useEffect } from "react";
 
 const KakaoMap = () => {
+  const [state, setState] = useState({
+    center: {
+      lat: 33.450701,
+      lng: 126.570667,
+    },
+    errMsg: null,
+    isLoading: true,
+  });
+
+  useEffect(() => {
+    if (navigator.geolocation) {
+      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setState((prev) => ({
+            ...prev,
+            center: {
+              lat: position.coords.latitude, // 위도
+              lng: position.coords.longitude, // 경도
+            },
+            isLoading: false,
+          }));
+        },
+        (err) => {
+          setState((prev) => ({
+            ...prev,
+            errMsg: err.message,
+            isLoading: false,
+          }));
+        }
+      );
+    } else {
+      // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+      setState((prev) => ({
+        ...prev,
+        errMsg: "geolocation을 사용할수 없어요..",
+        isLoading: false,
+      }));
+    }
+  }, []);
+
   return (
-    <Map
-      center={{ lat: 33.5563, lng: 126.79581 }}
-      style={{ width: "100%", height: "750px" }}
-    >
-      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-        <div style={{ color: "#000" }}>Hello World!</div>
-      </MapMarker>
+    <Map center={state.center} style={{ width: "100%", height: "100vh" }}>
+      {!state.isLoading && (
+        <MapMarker position={state.center}>
+          {/* <div style={{ color: "#000" }}></div> */}
+        </MapMarker>
+      )}
     </Map>
   );
 };
--- betweenus/src/pages/Map.js
+++ betweenus/src/pages/Map.js
@@ -5,14 +5,23 @@ import BottomNavigation from "../components/common/BottomNavigation";
 import KakaoMap from "../components/map/KakaoMap";
 
 const Map = () => {
+  const searchButton = () => {
+    console.log("search!");
+  };
+  const currentLocation = () => {
+    console.log("current!");
+  };
   return (
     <div className="Map">
       <MetaTag />
       <section className="map" width="100%" height="100%">
+        <a href="#" className="btn-search" onClick={searchButton}></a>
+        <button
+          type="button"
+          className="btn-location"
+          onClick={currentLocation}
+        ></button>
         <KakaoMap />
-        <a href="/search" className="btn-search">
-          <button type="button" className="btn-location"></button>
-        </a>
         <BottomNavigation />
       </section>
     </div>
