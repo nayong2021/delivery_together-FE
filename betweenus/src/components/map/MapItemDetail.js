d18c22afe263f386627a689205b2ac68a27b6f74
kwaksj329 <kwaksj329@gmail.com>
2022-10-17 01:04:16 +0900

    feat: Add MapItemDetail to show Item Details

--- betweenus/src/components/map/MapItemDetail.js
+++ betweenus/src/components/map/MapItemDetail.js
@@ -1,16 +1,19 @@
 import * as React from "react";
 import styled from "styled-components";
 import { GetJoinableGroupBuyingApi } from "../../modules/api/home/GetJoinableGroupBuyingApi";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import OrderTimeClock from "../common/OrderTimeClock";
 
 const MapItemDetail = (index) => {
-  console.log(index);
+  // console.log(index);
   const [list, setList] = useState([]);
+  const [clickedInfo, setInfo] = useState({});
+
   let temp;
   const ItemContainer = styled.div`
     position: absolute;
     top: 60%;
-    width: 85%;
+    width: 88%;
     height: 25%;
     border-radius: 24px 24px 24px 24px;
     box-shadow: 0 0 15px 0 rgba(0, 0, 0, 0.35);
@@ -21,11 +24,38 @@ const MapItemDetail = (index) => {
 
   const getList = async () => {
     temp = await GetJoinableGroupBuyingApi();
-    console.log(temp);
+    // console.log(index.index);
+    console.log(temp[index.index].additionalInfo);
     setList(temp);
-    console.log(list);
+    setInfo(temp[index.index]);
   };
-  return <ItemContainer></ItemContainer>;
+
+  useEffect(() => {
+    getList();
+  }, []);
+
+  return (
+    <>
+      <ItemContainer>
+        <div className="list-item">
+          <div className="tit">{clickedInfo.title}</div>
+          <div className="place">{clickedInfo.storeName}</div>
+          <div className="info-group">
+            <div className="time">
+              <OrderTimeClock timeToOrder={clickedInfo.timeToOrder} />
+            </div>
+            <div className="info">
+              <span className="price">
+                예상배달비: {clickedInfo.expectedDeliveryFee}원 /{" "}
+                {clickedInfo.currentParticipant}명
+              </span>
+              {/* <span className="distance">{clickedInfo.distance}</span> */}
+            </div>
+          </div>
+        </div>
+      </ItemContainer>
+    </>
+  );
 };
 
 export default MapItemDetail;
